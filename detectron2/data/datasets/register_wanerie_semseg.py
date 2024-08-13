bf5ea17a4ee43c3f4bcbe8d1ea4e9492e2811098
Hewage Dabare <h.dabare@ecu.edu.au>
2024-08-13 17:43:30 +0800

    Remove train and val

--- detectron2/data/datasets/register_wanerie_semseg.py
+++ detectron2/data/datasets/register_wanerie_semseg.py
@@ -29,32 +29,6 @@ def get_wanerie_dicts(img_dir, split_filename):
         dataset_dicts.append(record)
     return dataset_dicts
 
-for d in ["train", "val"]:
-    DatasetCatalog.register("wanerie_" + d, lambda d=d: get_wanerie_dicts("/home/hdabare/GANav-offroad/data/wanerie/", d + ".txt"))
-    MetadataCatalog.get("wanerie_" + d).set(stuff_classes=[
-        "BACKGROUND", 
-        "GRASS_TREE", 
-        "POLE", 
-        "TREE", 
-        "LEAVES", 
-        "FENCE", 
-        "LOG", 
-        "GRASS", 
-        "ROAD_SIGN", 
-        "SMALL", 
-        "GRAVEL", 
-        "GROUND", 
-        "HORIZON", 
-        "ROOTS", 
-        "SKY", 
-        "DELINEATOR", 
-        "ROCK", 
-        "ROAD"
-        ],
-            ignore_label=0,
-            thing_dataset_id_to_contiguous_id={0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 11:11, 12:12, 13:13, 14:14, 15:15, 16:16, 17:17})
-
-MetadataCatalog.get("wanerie_val").set(evaluator_type="sem_seg")
 DatasetCatalog.register("wanerie_test", lambda d=d: get_wanerie_dicts("/home/hdabare/GANav-offroad/data/wanerie/", "test.txt"))
 MetadataCatalog.get("wanerie_test").set(stuff_classes=[  "BACKGROUND", 
         "GRASS_TREE", 
